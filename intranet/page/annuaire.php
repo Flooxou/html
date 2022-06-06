58f01baff9163d8a26a4d63de4743846fc892eb7
BenJix <benjamin.mrl21@gmail.com>
2022-06-06 19:47:32 +0200

    update06062022 V2

--- intranet/page/annuaire.php
+++ intranet/page/annuaire.php
@@ -6,43 +6,175 @@
     getHeader();
 
     getNav();
+?>
+        <br><br><br>
 
-echo    '<div class="row">
+        <div class="text-center titre">
+            <h1>Notre PDG</h1>
+        </div>
+        
+        <br><br>
+
+        <div class="row">
+            <div class="col-sm-3">
+            </div>
             <div class="col-sm-4">
+                <ul>   
+                    <div class="card cardCeo">
+                        <img class="card-img-top" src="../img/annuaire/ceo.jpeg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Guy Don De Course</h4>
+                            <p class="card-text text-white">DRH of LLSM</p>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+        <br><br><br><br><br><br><br><br><br><br>
+
+        <div class="text-center titre">
+            <h2>Personnes importantes</h2>
+        </div>
+        
+        <br><br>
+
+        <div class="row">
+            <div class="col-sm-2">
+            </div>
+            <div class="col-sm-5">
                 <ul>
-                    <div class="card" style="width:400px">
+                    <div class="card cardBoss">
                         <img class="card-img-top" src="../img/annuaire/2nd.jpg" alt="Card image">
                         <div class="card-img-overlay">
-                            <h4 class="card-title text-white">Florent Pastis</h4>
+                            <h4 class="card-title text-white">Marc Des Points</h4>
                             <p class="card-text text-white">Seconde of LLSM</p>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+            <div class="col-sm-1">
+                <ul>
+                    <div class="card cardBoss">
+                        <img class="card-img-top" src="../img/annuaire/drh.jpeg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Daisy D. Malplacé</h4>
+                            <p class="card-text text-white">DRH of LLSM</p>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+        <br><br><br><br><br><br><br><br><br><br>
+
+        <div class="text-center titre">
+            <h2>Nos collaborateurs</h2>
+        </div>
+        
+        <br><br><br><br>
+
+        <div class="row">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-4">
+                <ul>
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/femme1.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Camille Onette</h4>
                         </div>
                     </div>
                 </ul>
             </div>
             <div class="col-sm-4">
                 <ul>   
-                    <div class="card" style="width:400px">
-                        <img class="card-img-top" src="../img/annuaire/ceo.jpeg" alt="Card image">
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/homme2.jfif" alt="Card image">
                         <div class="card-img-overlay">
-                            <h4 class="card-title text-white">Pascal Obistro</h4>
-                            <p class="card-text text-white">DRH of LLSM</p>
+                            <h4 class="card-title text-white">Eddy Don Marcel</h4>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+            <div class="col-sm-1">
+                <ul>
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/femme2.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Anne Héantie</h4>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="row">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-4">
+                <ul>
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/homme1.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Laurent Gina</h4>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+            <div class="col-sm-5">
+                <ul>   
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/homme3.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Jean Bambois</h4>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="row">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-4">
+                <ul>
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/femme3.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Marie Viere</h4>
                         </div>
                     </div>
                 </ul>
             </div>
             <div class="col-sm-4">
-                <ul>
-                    <div class="card" style="width:400px">
-                        <img class="card-img-top" src="../img/annuaire/drh.jpeg" alt="Card image">
+                <ul>   
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/homme4.jpg" alt="Card image">
                         <div class="card-img-overlay">
-                            <h4 class="card-title text-white">Brigitte BarreDo</h4>
-                            <p class="card-text text-white">DRH of LLSM</p>
+                            <h4 class="card-title text-white">Alain Terrieure</h4>
                         </div>
                     </div>
                 </ul>
             </div>
-        </div>';
+            <div class="col-sm-1">
+                <ul>
+                    <div class="card cardEmploye">
+                        <img class="card-img-top" src="../img/annuaire/femme4.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h4 class="card-title text-white">Jessica Scroute</h4>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
 
-    getFooter();
-    
+        <br><br><br><br>
+
+<?php
+    getFooter();   
 ?>
